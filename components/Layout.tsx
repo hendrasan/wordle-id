d1b287f6d770efe372bad0e865131f12d0587951
Hendra Susanto <anotherhendrasusanto@gmail.com>
2022-01-20 22:44:12 +0700

    chore: continuation of last commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,5 @@
 import clsxm from '@/lib/clsxm';
 import React from 'react';
-import Header from './Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
