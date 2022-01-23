3145db020b89037f5b2a88d7fa633d9ca3cbec4e
Hendra Susanto <anotherhendrasusanto@gmail.com>
2022-01-23 14:29:31 +0700

    remove analytics for now

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,23 +29,6 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=optional'
             rel='stylesheet'
           />
-
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-    
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-              `,
-            }}
-          />
         </Head>
         <body>
           <Main />
